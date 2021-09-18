c265bf93d8551fb207f9d860dd9759d062abe657
ryotaro_seto <hahahahawaiwai321@gmail.com>
2021-09-19 00:45:03 +0900

    vueでサイドバー作成

---
+++ vue-sidbar/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Sidebar />
+  </div>
+</template>
+
+<script>
+  import Sidebar from './components/Sidebar.vue'
+
+  export default {
+    components: {
+      Sidebar
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Anton');
+  @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Anton', Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    letter-spacing: 1px;
+    color: #322F42;
+    width: 100%;
+    min-height: 100vh;
+    background: url('./assets/app_bg.jpg') center center;
+    background-size: cover;
+    overflow: hidden;
+  }
+</style>
